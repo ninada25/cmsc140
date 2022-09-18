42b67bda292a1029b581aca93da09a4ac6368d64
Nina Austria <ninaaustria@Ninas-MacBook-Air.local>
2022-09-18 13:17:45 -0500

    First commit

---
+++ Desktop/py_scripts/class_lecture_notes/wk1-vars.py
@@ -0,0 +1,35 @@
+### Wednesday, Class 2 (9/14/22):
+
+print(5+4)
+
+x = 5+4
+print(x)
+
+snake_case_name = "This is a snake."
+camelCaseName = "This is a camel."
+
+print(camelCaseName)
+
+print(7/2)
+
+apples = 20
+print("I ate " + str(apples) + " apples.")
+
+# pears = int("pears") - will throw a ValueError!
+pears_length = len("pears")
+print(pears_length)
+
+# Prompt user input
+print("Please say something: ")
+my_response = input()
+print("you said: " + my_response)
+
+print([3,4,5,6]) # you can print a list in R! this sets it apart from a lot of other languages
+
+# User Input
+
+# We want to give someone the square of a number.
+print("Please enter a number: ")
+num = int(input())
+square = num ** 2
+print("The square of your number is " + str(square))
---
+++ Desktop/py_scripts/homework/austria_q2.py
@@ -0,0 +1,7 @@
+# Random number guesser
+
+x = 3 # assign a value of 3 to the variable x
+print("Welcome to the random number guesser.")
+print("Please input a number between 1 and 10: ") # prompt user to input a number
+num = input() # assign user input to the variable num
+print("You chose " + str(num) + ". The number I chose was " + str(x) + ". Did you guess correctly?")
