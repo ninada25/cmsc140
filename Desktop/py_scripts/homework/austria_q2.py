393ef7f4f6fb9fd9250dd00aeeefbf672c4fcd5d
Nina Austria <ninaaustria@Ninas-MacBook-Air.local>
2022-09-19 22:33:30 -0500

    Added homework 1 code

--- Desktop/py_scripts/homework/austria_q2.py
+++ Desktop/py_scripts/homework/austria_q2.py
@@ -1,7 +1,7 @@
 # Homework 1 - Q2: A Terrible Number Guesser (9/22/22)
 
-# This program has a variable set to a number. It asks a user for a number between 1 and 10, 
-# and prints both numbers. It then asks the user to compare the numbers and see if they guessed correctly.
+# This program has a variable set to a number. It asks a user for a number between 1 and 10, and prints both numbers. 
+# It then asks the user to compare the numbers and see if they guessed correctly.
 
 num = 3 
 print("Welcome to the random number guesser.")
